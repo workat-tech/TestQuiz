c3c07ee5feb6440150f632fdc18d0c37df29deeb
Gaurav Chandak <gaurav@workat.tech>
2021-01-17 07:54:42 +0000

    Add new line after question

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const readLineSync = require('readline-sync');
-const name = readLineSync.question('What is your name?');
+const name = readLineSync.question('What is your name?\n');
 console.log(`Hello ${name}`);
